026ab7e04f4d4e4b245197500b3da566f8850621
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-08-22 10:50:01 -0500

    3.5.10.1

--- src/ScintillaNET/Properties/AssemblyInfo.cs
+++ src/ScintillaNET/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.10.0")]
-[assembly: AssemblyFileVersion("3.5.10.0")]
+[assembly: AssemblyVersion("3.5.10.1")]
+[assembly: AssemblyFileVersion("3.5.10.1")]
 [assembly: AssemblyInformationalVersion("3.5.10")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
